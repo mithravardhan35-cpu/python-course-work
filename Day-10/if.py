2ae5e5cf40bcf439829b57bf9fe52ca207740e1c
mithravardhan35-cpu <mithravardhan35@gmail.com>
2026-08-21 18:00:00 +0530

    if.py,ifelse.py,

---
+++ Day-10/if.py
@@ -0,0 +1,17 @@
+sales = int(input("Enter the sales:"))
+
+if sales > 1000:
+     print("Best Seller")
+     print("Best Seller")
+
+eli_acc = eval(input("English Account"))
+ver_sub = eval(input("Meta Verified Subscription:"))
+
+if eli_acc and ver_sub:
+    print("Verified Badge Granted")
+
+rain_status = eval(input("Enter the rain status:"))
+
+if rain_status:
+    print("Extra Rain Charges Applied")
+           
